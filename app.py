20b1f564210c7a482e16f892eb644cd7b3eb473c
Alyssa Romero <allie642@uab.edu>
2019-07-05 09:48:26 -0500

    Basic UI Main Screen

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+topics = [
+	{"title": "Animals"},
+	{"title": "School"},
+	{"title": "Traveling"},
+	{"title": "Home"}
+]
+
 @app.route("/")
 @app.route("/login")
 def login():
@@ -10,6 +17,10 @@ def login():
 @app.route("/register")
 def register():
 	return render_template("register.html")
+
+@app.route("/main")
+def main():
+	return render_template("main.html", topics=topics, length=len(topics))
 
 if __name__ == "__main__":
 	app.run(debug=True)
---
+++ static/actions.js
@@ -0,0 +1,19 @@
+alertUser() {
+  alert("You Have Pressed Add Button");
+}
+
+function addTopic(title) {
+
+}
+
+function removeTopic(title) {
+
+}
+
+function addCard(front, back) {
+
+}
+
+function removeCard(front, back) {
+
+}
